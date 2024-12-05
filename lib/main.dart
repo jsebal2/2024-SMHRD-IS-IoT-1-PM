56571ed4ceb88193404d6c46e9fc4a6c375d6b16
daa <k01997@naver.com>
2024-12-05 09:13:09 +0900

    다영mainpage

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: Login(),
+      home: Mainpage(),
     );
   }
 }
